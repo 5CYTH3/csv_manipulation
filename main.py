71d0463828d2037b597f4dcf54d26c206e008aec
Chateauvisionn <89737965+Chateauvisionn@users.noreply.github.com>
2022-03-23 12:41:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ import matplotlib.pyplot as mp
 import matplotlib as m
 import pandas as p
 
-
-def read_data():
-    set = p.read_csv("dataset.csv")
-    scores = set['score'].sort_values(ascending=True)
-    mp.plot()
-    mp.show()
-
-
-read_data()
+set = p.read_csv("dataset.csv")
+scores = set['score'].sort_values(ascending=True)
+mp.plot()
+mp.show()
