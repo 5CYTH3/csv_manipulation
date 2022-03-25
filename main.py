0a73b2976705b5e450a7b0646c99d9884414d878
5CYTH3 <ScytheD@outlook.fr>
2022-03-25 11:46:20 +0100

    Added docstring in the txns_graph() function and trying to make things work

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ import matplotlib.pyplot as plt
 # Mathis : Sales
 # Me : Transactions (txns)
 
-def txns_graph():
-    data = p.read_csv('nft_sales.csv')
-    x = data['Txns']
-    y = data[]
-    plt.plot()
+def txns_graph(data: p.DataFrame):
+    """
+    This function print a chart with the 10 Collections with the most transactions 
+    in the dataset, with their height the Txns column associated values.
+    @param: p.DataFrame
+    """
+    y = data['Txns'].head(10)
+    x = data['Collections'].head(10)
+    plt.bar(x, y)
+    plt.show()
     
 def main():
-    txns_graph()
+    data = p.read_csv('nft_sales.csv')
+    txns_graph(data)
     
 
 if __name__ == "__main__":
