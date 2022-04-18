6ec988db2c6b64e000dda2225f53eae3af6f4c84
5CYTH3 <ScytheD@outlook.fr>
2022-04-18 20:19:05 +0200

    Fixed bug with figure title and figure showing by moving the figure_id assignment.

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from matplotlib.container import BarContainer
 import pandas as p
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, matplotlib.container as plb
 
 # Enzo : Owners
 # Mathis : Sales
 # Me : Transactions (txns)
 
 
-def show_bar_chart(data: p.DataFrame, name: str, color: str, fig_id: int) -> BarContainer:
+def plot_bar_chart(data: p.DataFrame, name: str, color: str, fig_id: int) -> plb.BarContainer:
     """
     This function shows a bar chart with y datas of the 10 first NFT collection.
     @param: data<DataFrame>, name<str>, color<str>, fig_id<int>
@@ -16,23 +15,23 @@ def show_bar_chart(data: p.DataFrame, name: str, color: str, fig_id: int) -> Bar
     ordered_data = data.sort_values(name)
     y = ordered_data[name].head(10)
     x = ordered_data['Collections'].head(10)
-    plt.title(name)
     f = plt.figure(fig_id)
+    plt.title(name)
     return plt.bar(x=x, height=y, color=color)
     
 
 
-def show_pie_chart(data: p.DataFrame, name: str, fig_id: int) -> any:
+def plot_pie_chart(data: p.DataFrame, name: str, fig_id: int) -> any:
     """
     This function shows a pie chart with data of the first 5 NFT collections.
     @param: data<DataFrame>, name<str>
     @return: matplotlib pie chart <any>
     """
+    f = plt.figure(fig_id)
     ordered_data = data.sort_values(name)
     plt.title(name)
     labels = ordered_data['Collections'].head(5)
     values = ordered_data[name].head(5)
-    f = plt.figure(fig_id)
     try:
         return plt.pie(values, labels=labels)
     except ValueError:
@@ -43,11 +42,11 @@ def show_pie_chart(data: p.DataFrame, name: str, fig_id: int) -> any:
 def main():
     data = p.read_csv('nft_sales.csv')
     # Show transactions
-    show_bar_chart(data, 'Txns', 'blue', 1)
+    plot_bar_chart(data, 'Txns', 'blue', 1)
     # Show Sales
-    show_pie_chart(data, 'Sales', 2)
+    plot_pie_chart(data, 'Sales', 2)
     # Show Owners
-    show_bar_chart(data, 'Owners', 'green', 3)
+    plot_bar_chart(data, 'Owners', 'green', 3)
     plt.show()
 
 
